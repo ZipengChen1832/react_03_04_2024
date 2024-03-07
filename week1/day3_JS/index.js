3d89ce411f93544d9a6c04b6f533ab036f843ff0
Brandon Chen <zipengchen1832@gmail.com>
2024-03-07 15:06:11 -0500

    day 3 js basics

---
+++ week1/day3_JS/index.js
@@ -0,0 +1,114 @@
+//DOM Manipulation
+
+// const titleElement = document.querySelector(".title")
+
+// document.querySelectorAll
+
+// let count = 0;
+
+// button.addEventListener("click", ()=>{
+//     count = count + 1;
+//     titleElement.textContent = count;
+// })
+
+// var, let, const
+
+// var str = "qwjdioqw";
+// var num = 1;
+// var bool = true;
+// var und = undefined;
+// var nul = null;
+
+// console.log(str, typeof str);
+// console.log(num, typeof num);
+// console.log(bool, typeof bool);
+// console.log(und, typeof und);
+// console.log(nul, typeof nul);
+
+//hoisting
+// console.log(todo)
+// var todo = "learn coding"
+
+// let a = 0;
+// a = 12412
+
+// const b = 124
+// b = 192847124
+
+// const obj = {
+//   a: "a",
+//   b: "b",
+// };
+
+// const obj1 = {
+//   a: "a",
+//   b: "b",
+// };
+// const arr = [1, 2, 3, 4];
+
+// const keyName = "a"
+// console.log(obj[keyName]);
+
+// const length = arr.length;
+// console.log(arr[length - 1]);
+
+// const inputElem1 = document.querySelector(".input1");
+// const inputElem2 = document.querySelector(".input2");
+// const button = document.querySelector(".click-btn");
+
+// button.addEventListener("click", () => {
+//   const value1 = Number(inputElem1.value);
+//   const value2 = Number(inputElem2.value);
+//   console.log("value1", typeof value1, value1);
+//   console.log("value2", typeof value2, value2);
+//   console.log("result", value1 + value2);
+// });
+
+// const person = {
+//     name: "joe"
+// }
+
+// if (person.age) {
+//   console.log("truthy");
+// } else {
+//   console.log("falsy");
+// }
+
+// if (!1 == 1) {
+//   console.log("truthy");
+// } else {
+//   console.log("falsy");
+// }
+
+// const type = "A"
+
+// switch(type){
+//     case "A":
+//         console.log("the type is A")
+//         break;
+//     case "B":
+//         console.log("the type is B")
+//         break;
+//     case "C":
+//         console.log("the type is C")
+//         break;
+//     default:
+//         console.log("unknown type")
+//         break;
+// }
+
+// const scoped = "outside";
+
+// function foo() {
+//   //   const scoped = "inside";
+//   const privateVar = "private";
+//   console.log(scoped);
+// }
+
+// foo();
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// console.log(sum(1,2));

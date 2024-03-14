c653da6820eb7e6dcf780b97bec207ff25de1956
Brandon Chen <zipengchen1832@gmail.com>
2024-03-14 15:31:15 -0400

    mvc finish, instruction for evaluation

---
+++ week1/day9_todolist/evalution-prep.md
@@ -0,0 +1,36 @@
+Requirements:
+Use the MVC Pattern to write the app
+1:30pm to 5:30pm, 4 hours
+
+submit a repo link at around 5:20pm est
+
+we care about the last commit
+(if you committed your code around 4pm, but forgot and submitted at 5:35, that's fine)
+
+make sure your first commit is before the deadline
+
+if you want to improve after deadline, you're welcome to do so
+
+use a json server as the database
+
+
+
+Guides:
+
+Javascript:
+DOM Manipulation
+arrays / objects
+class, prototypes, 
+async / promise / API calling
+
+CSS:
+flex / grid
+box model
+colors
+
+
+Evaluation:
+Focus on JS first before CSS
+
+if you're stuck on a small thing, move on
+
--- week1/day9_todolist/index.js
+++ week1/day9_todolist/index.js
@@ -18,9 +18,16 @@ const todosAPIs = (function () {
     }).then((res) => res.json());
   }
 
+  async function deleteTodo(id) {
+    return fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+    }).then((res) => res.json());
+  }
+
   return {
     getTodos,
     addTodo,
+    deleteTodo,
   };
 })();
 
@@ -31,11 +38,19 @@ class TodosView {
     this.todoList = document.querySelector(".todo-list");
   }
 
+  clearInput() {
+    this.todoInput.value = "";
+  }
+
   renderTodos(todos) {
     this.todoList.innerHTML = "";
     todos.forEach((todo) => {
       this.renderNewTodo(todo);
     });
+  }
+
+  removeTodoElem(id) {
+    document.getElementById(id).remove();
   }
 
   renderNewTodo(newTodo) {
@@ -55,7 +70,28 @@ class TodosView {
   }
 }
 
-class TodosModel {}
+class TodosModel {
+  #todos;
+  constructor(todos = []) {
+    this.#todos = todos;
+  }
+
+  getTodos() {
+    return this.#todos;
+  }
+
+  setTodos(newTodos) {
+    this.#todos = newTodos;
+  }
+
+  addTodo(newTodo) {
+    this.#todos.push(newTodo);
+  }
+
+  deleteTodo(id) {
+    this.#todos = this.#todos.filter((todo) => todo.id !== id);
+  }
+}
 
 class TodosController {
   constructor(view, model) {
@@ -71,23 +107,45 @@ class TodosController {
 
   setUpEvents() {
     this.setUpSubmitEvent();
+    this.setUpDeleteEvent();
   }
 
   async fetchTodos() {
     const todos = await todosAPIs.getTodos();
+    this.model.setTodos(todos);
     this.view.renderTodos(todos);
+  }
+
+  setUpDeleteEvent() {
+    //event delegation
+    this.view.todoList.addEventListener("click", async (e) => {
+      const elem = e.target;
+      if (elem.classList.contains("todo__del-btn")) {
+        const todoElem = elem.parentElement.parentElement;
+        const deleteId = todoElem.id;
+        await todosAPIs.deleteTodo(deleteId);
+        this.model.deleteTodo(deleteId);
+        this.view.removeTodoElem(deleteId);
+      }
+    });
   }
 
   setUpSubmitEvent() {
     this.view.newTodoForm.addEventListener("submit", async (e) => {
       e.preventDefault();
-      const title = this.view.todoInput.value;
+      const input = this.view.todoInput;
+      const title = input.value;
+      if (!title) {
+        return;
+      }
+
       const newTodo = await todosAPIs.addTodo({
         title,
       });
-      console.log(newTodo);
-
+      this.model.addTodo(newTodo);
+      //   console.log(this.model.getTodos());
       this.view.renderNewTodo(newTodo);
+      this.view.clearInput();
     });
   }
 }
@@ -95,3 +153,6 @@ class TodosController {
 const todosView = new TodosView();
 const todosModel = new TodosModel();
 const todosController = new TodosController(todosView, todosModel);
+
+
+
